9d46cfe6da804d5ce497f0f68e157b88581a0565
The Code Destroyer <the.code.destroyer@gmail.com>
2015-01-06 21:33:35 +0100

    Created a generic API route, for now

--- server/routes.js
+++ server/routes.js
@@ -1,21 +1,68 @@
-var Setting = require('./models/setting');
+var Workout = require('./models/workout');
 
 module.exports = function(app) {
 
-    app.get('/api/setting', function(req, res) {
-        Setting.find(function(err, settings) {
+    app.get('/api/*', function(req, res) {
+        var splitPath = req.path.split('/');
 
-            if (err)
-                res.send(err);
+        if (splitPath.length !== 3) {
+            res.send({ error: 'Internal server error' });
+            console.log('1');
+        }
+        else {
+            var Model = require('./models/' + splitPath[2]);
+            Model.find(function (err, array) {
 
-            console.log(settings);
+                if (err)
+                    res.send(err);
 
-            res.json(settings);
-        });
+                var wrappedData = wrapPureData(array);
+
+                res.json(wrappedData);
+            });
+
+        }
+    });
+
+    app.post('/api/*', function(req, res) {
+        var splitPath = req.path.split('/');
+
+        if (splitPath.length !== 3) {
+            res.send({ error: 'Internal server error' });
+            console.log('1');
+        }
+        else {
+            var Model = require('./models/' + splitPath[2]);
+            Model.find(function (err, array) {
+
+                if (err)
+                    res.send(err);
+
+
+                var response = {
+                    now: "ok",
+                    updates: getEpoch()
+                };
+
+                res.json(response);
+            });
+
+        }
     });
 
     app.get('/', function(req, res) {
         res.sendFile(__dirname + '/app/index.html');
     });
+
+    function wrapPureData(array) {
+        return {
+            now: getEpoch(),
+            updates: array
+        };
+    }
+
+    function getEpoch() {
+        return new Date().getTime();
+    }
 
 };
