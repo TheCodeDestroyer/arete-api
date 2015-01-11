1bff507fd67129a8b43025e9a11a8e13d0a9fb21
The Code Destroyer <the.code.destroyer@gmail.com>
2015-01-11 18:38:06 +0100

    Added CORS support

--- server.js
+++ server.js
@@ -12,11 +12,25 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
 mongoose.set('debug', dev);
 mongoose.connect(dev ? db.devUrl : db.url);
 
+var allowCors = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
+app.use(allowCors);
 
 require('./server/routes')(app);
 app.listen(port);
